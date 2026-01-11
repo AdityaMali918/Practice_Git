ff6becde4c83058569aee9f1d816085a14316e2c
Aditya <adityamali918@gmail.com>
2026-01-11 07:21:27 +0530

    New files are added

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import functools
+
 def add(*a):
    print(type(a))
    sum_number = sum(a)
-   print(sum_number)
+   print(f'Sum: {sum_number}')
+
+def multiply(*b):
+   ans = functools.reduce(lambda x,y: x*y ,b)
+   print(f'Multiply: {ans}')
 
 if __name__ == "__main__":
    add(1,2,3)   
